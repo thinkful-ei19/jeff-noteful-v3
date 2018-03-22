834258d092f5338249bfcbd9b43a36014ed4f265
Jeff Vega <jeff@intellibliss.com>
2018-03-21 18:32:45 -0700

    files

---
+++ routes/notes.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const express = require('express');
+// Create an router instance (aka "mini-app")
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// const { PORT, MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+/* ========== GET/READ ALL ITEM ========== */
+router.get('/notes', (req, res, next) => {
+  const searchTerm = req.query.searchTerm;
+  let filter = {};
+
+  if (searchTerm) {
+    const re = new RegExp(searchTerm, 'i');
+    filter.title = { $regex: re };
+  }
+
+  return Note.find(filter)
+    .sort('created')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(next);
+});
+ 
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/notes/:id', (req, res, next) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+  Note.findById(id)
+    .then(result => {
+      if(result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/notes', (req, res, next) => {
+  const {title, content} = req.body;
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = { title, content };
+
+  Note.create(newItem)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateItem = { title, content };
+  const options = { new: true };
+
+  Note.findByIdAndUpdate(id, updateItem, options)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/notes/:id', (req, res, next) => {
+
+  const { id } = req.params;
+
+  Note.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { PORT, MONGODB_URI } = require('./config'); //creating port variable
+//extracting values from require.config
+
+const notesRouter = require('./routes/notes');
+
+// Create an Express application
+const app = express();
+
+// Log all requests. Skip logging during
+app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
+  skip: () => process.env.NODE_ENV === 'test'
+}));
+
+// Create a static webserver
+app.use(express.static('public'));
+
+// Parse request body
+app.use(express.json());
+
+// Mount router on "/api"
+app.use('/api', notesRouter);
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+
+if (require.main === module) {
+  //connecting to db and either success message or error
+  mongoose.connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app;
